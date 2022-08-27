6ec624a5ab2775edf1021600182a100aad60f31c
tesst123 <test>
2022-08-27 16:48:55 +0530

    VPC corrected3

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region = "us-east-1"
+
 }
+
 #Create the VPC
  resource "aws_vpc" "Main" {                # Creating VPC here
    cidr_block       = "${var.main_vpc_cidr}"     # Defining the CIDR block use 10.0.0.0/24 for demo
